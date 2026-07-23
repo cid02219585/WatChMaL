41b1e80d9861960683da69e39c4b39020782bf31
Stephanie Cannon <sc4422@lx05.hep.ph.ic.ac.uk>
2026-07-23 18:47:43 +0100

    competitive slot attention

--- watchmal/model/gnn_multi_ring_enc_dec.py
+++ watchmal/model/gnn_multi_ring_enc_dec.py
@@ -531,12 +531,17 @@ class TransformerDecoderLayer(nn.Module):
                 x = x + self._sa_block(
                     self.norm1(x), tgt_mask, tgt_key_padding_mask, tgt_is_causal
                 )
+                # x = x + self._mha_block(
+                #     self.norm2(x),
+                #     memory,
+                #     memory_mask,
+                #     memory_key_padding_mask,
+                #     memory_is_causal,
+                # )
                 x = x + self._mha_block(
                     self.norm2(x),
                     memory,
-                    memory_mask,
                     memory_key_padding_mask,
-                    memory_is_causal,
                 )
                 x = x + self._ff_block(self.norm3(x))
             else:
